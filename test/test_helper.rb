afa6a277818d0e9600a8998a0f9b8493b4a46756
Jonathan Baker <jonathan@jmb.dev>
2024-10-01 22:02:18 -0400

    Add simple request fixtures.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,3 +3,15 @@
 require "minitest/autorun"
 require "minitest/declarative"
 require "proto_plugin"
+
+def requests(name)
+  file = File.join("test", "fixtures", "#{name}.pb")
+
+  unless File.exist?(file)
+    raise "fixture not found - #{file}"
+  end
+
+  Google::Protobuf::Compiler::CodeGeneratorRequest.decode(
+    File.read(file),
+  )
+end
